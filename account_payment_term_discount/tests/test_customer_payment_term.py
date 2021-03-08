c1ffb0385f8419d0321a249294861ee55c83b03d
Daniel Reis <dreis@opensourceintegrators.com>
2021-03-08 16:51:43 +0000

    [FIX] account_payment_term_discount: fix tests

--- account_payment_term_discount/tests/test_customer_payment_term.py
+++ account_payment_term_discount/tests/test_customer_payment_term.py
@@ -6,9 +6,9 @@ from odoo import fields
 from odoo.tests import common
 
 
-class TestLandedCostCompanyPercentage(common.TransactionCase):
+class TestPaymentTermDiscount(common.TransactionCase):
     def setUp(self):
-        super(TestLandedCostCompanyPercentage, self).setUp()
+        super().setUp()
 
         # Refs
         self.main_company = self.env.ref("base.main_company")
@@ -54,6 +54,20 @@ class TestLandedCostCompanyPercentage(common.TransactionCase):
             )
         )
 
+        # Income account
+        self.income_account = self.account_model.with_user(
+            self.account_manager.id
+        ).search(
+            [
+                (
+                    "user_type_id",
+                    "=",
+                    self.env.ref("account.data_account_type_revenue").id,
+                )
+            ],
+            limit=1,
+        )
+
         # Create receivable account
         self.account_rec1_id = self.account_model.with_user(
             self.account_manager.id
@@ -66,32 +80,6 @@ class TestLandedCostCompanyPercentage(common.TransactionCase):
             )
         )
 
-        # Prepare invoice line values
-        self.invoice_line_data = [
-            (
-                0,
-                0,
-                {
-                    "product_id": self.env.ref("product.product_product_5").id,
-                    "quantity": 10.0,
-                    "account_id": self.env["account.account"]
-                    .search(
-                        [
-                            (
-                                "user_type_id",
-                                "=",
-                                self.env.ref("account.data_account_type_revenue").id,
-                            )
-                        ],
-                        limit=1,
-                    )
-                    .id,
-                    "name": "product test 5",
-                    "price_unit": 100.00,
-                },
-            )
-        ]
-
         # Create Payment term
         self.payment_term = self.payment_term_model.with_user(
             self.account_manager.id
@@ -99,7 +87,7 @@ class TestLandedCostCompanyPercentage(common.TransactionCase):
             dict(
                 name="5%10 NET30",
                 is_discount=True,
-                note="5% discount if payment done within 10days, " "otherwise net",
+                note="5% discount if payment done within 10 days, otherwise net",
                 line_ids=[
                     (
                         0,
@@ -109,93 +97,76 @@ class TestLandedCostCompanyPercentage(common.TransactionCase):
                             "discount": 5.0,
                             "discount_days": 10,
                             "discount_expense_account_id": self.account_discount_id.id,
-                            "days": 10,
+                            "days": 30,
                         },
                     )
                 ],
             )
         )
 
-    def test_customer_invoice_payment_term_discount(self):
-        """Test customer invoice and payment term discount"""
-
-        # Create customer invoice and verify workflow with discount
+        # Create customer invoice
         self.customer_invoice = self.account_invoice_model.with_user(
             self.account_manager.id
         ).create(
             dict(
                 name="Test Customer Invoice",
-                reference_type="none",
-                payment_term_id=self.payment_term.id,
+                move_type="out_invoice",
+                invoice_date=fields.Date.today(),
+                invoice_payment_term_id=self.payment_term.id,
                 journal_id=self.journalrec.id,
                 partner_id=self.partner_id.id,
-                account_id=self.account_rec1_id.id,
-                invoice_line_ids=self.invoice_line_data,
             )
         )
-        self.assertEquals(self.customer_invoice.state, "draft")
+        # Prepare invoice line values
+        self.customer_invoice.write(
+            {
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.env.ref("product.product_product_5").id,
+                            "quantity": 10.0,
+                            "account_id": self.income_account.id,
+                            "name": "product test 5",
+                            "price_unit": 100.00,
+                        },
+                    )
+                ]
+            }
+        )
         # Validate customer invoice
-        self.customer_invoice.action_invoice_open()
-        self.assertEquals(self.customer_invoice.state, "open")
+        self.customer_invoice.action_post()
+
+    def _do_payment(self, invoice, amount, date):
+        """
+        Create Payment wizard helper function
+        """
+        ctx = {
+            "active_ids": [invoice.id],
+            "active_id": invoice.id,
+            "active_model": "account.move",
+        }
+        PaymentWizard = self.env["account.payment.register"]
+        view = "account.view_account_payment_register_form"
+        with common.Form(PaymentWizard.with_context(ctx), view=view) as f:
+            f.amount = amount
+            f.payment_date = date
+        payment = f.save()
+        return payment.action_create_payments()
+
+    def test_customer_invoice_payment_term_discount(self):
+        """Test customer invoice and payment term discount"""
         # Update payment date that's match with condition within 10 days
-        invoice_date = fields.Date.from_string(self.customer_invoice.date_invoice)
-        payment_date = invoice_date + relativedelta(days=9)
-        # Create customer payment
-        payment_vals = {
-            "invoice_ids": [(6, 0, [self.customer_invoice.id])],
-            "invoice_id": self.customer_invoice.id,
-            "amount": 950.0,
-            "payment_date": payment_date,
-            "communication": self.customer_invoice.number,
-            "partner_id": self.partner_id.id,
-            "partner_type": "customer",
-            "journal_id": self.journalrec.id,
-            "payment_type": "inbound",
-            "payment_method_id": self.payment_method.id,
-        }
-        payment = self.payment_model.create(payment_vals)
-        payment.onchange_payment_amount()
-        payment.post()
+        payment_date = self.customer_invoice.invoice_date + relativedelta(days=9)
+        self._do_payment(self.customer_invoice, 950.0, payment_date)
         # Verify that invoice is now in Paid state
-        self.assertEquals(self.customer_invoice.state, "paid")
+        self.assertEqual(self.customer_invoice.payment_state, "in_payment")
 
-        # Create customer invoice and verify workflow without discount
-        self.customer_invoice2 = self.account_invoice_model.with_user(
-            self.account_manager.id
-        ).create(
-            dict(
-                name="Test Customer Invoice",
-                reference_type="none",
-                payment_term_id=self.payment_term.id,
-                journal_id=self.journalrec.id,
-                partner_id=self.partner_id.id,
-                account_id=self.account_rec1_id.id,
-                invoice_line_ids=self.invoice_line_data,
-            )
-        )
-        self.assertEquals(self.customer_invoice2.state, "draft")
-        # Validate customer invoice
-        self.customer_invoice2.action_invoice_open()
-        self.assertEquals(self.customer_invoice2.state, "open")
-        # Update payment date after 15 days of invoice date
-        invoice_date = fields.Date.from_string(self.customer_invoice2.date_invoice)
-        payment_date = invoice_date + relativedelta(days=15)
-        # Create customer payment
-        payment_vals = {
-            "invoice_ids": [(6, 0, [self.customer_invoice2.id])],
-            "invoice_id": self.customer_invoice2.id,
-            "amount": 950.0,
-            "payment_date": payment_date,
-            "communication": self.customer_invoice2.number,
-            "partner_id": self.partner_id.id,
-            "partner_type": "customer",
-            "journal_id": self.journalrec.id,
-            "payment_type": "inbound",
-            "payment_method_id": self.payment_method.id,
-        }
-        payment = self.payment_model.create(payment_vals)
-        payment.onchange_payment_amount()
-        payment.post()
-
-        # Verify that invoice is still in Open state
-        self.assertEqual(self.customer_invoice2.state, "open")
+    def test_customer_invoice_payment_term_no_discount(self):
+        """ Create customer invoice and verify workflow without discount """
+        # Update payment date that does not match with condition within 10 days
+        payment_date = self.customer_invoice.invoice_date + relativedelta(days=15)
+        self._do_payment(self.customer_invoice, 950.0, payment_date)
+        # Verify that invoice is now in Partial state
+        self.assertEqual(self.customer_invoice.payment_state, "partial")
